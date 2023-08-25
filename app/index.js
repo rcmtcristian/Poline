0fa4deb9deea0e040f469c946ffbd3781fa5c8e7
rcmtcristian <rcmtcristian@gmail.com>
2023-08-25 18:48:00 -0400

    💄 Style: fixed fonts and part of the js
    the issue with the fonts was that it was using a deprecated form of iplementation

--- app/index.js
+++ app/index.js
@@ -1,5 +1,32 @@
-// import math from 'utils/math';
-import Basilisk from 'images/basilogo.png';
-// if (IS_DEVELOPMENT) {
-//   console.log('Development mode', Basilisk);
-// }
+import Home from './pages/home';
+import Collections from './pages/collections';
+import Detail from './pages/detail';
+import About from './pages/about';
+
+class app {
+  constructor() {
+    this.createContent();
+    this.createPages();
+  }
+
+  createContent() {
+    this.content = document.querySelector('.content');
+    this.template = this.content.getAttribute('data-template');
+  }
+
+  createPages() {
+    this.pages = {
+      home: new Home(),
+      collections: new Collections(),
+      detail: new Detail(),
+      about: new About(),
+    };
+
+    this.page = this.pages[this.template];
+    this.page.create();
+
+    console.log(this.pages);
+  }
+}
+
+new app();
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const path = require("path"); //to access built-in path module
-const webpack = require("webpack"); //to access built-in plugins
+const path = require('path'); //to access built-in path module
+const webpack = require('webpack'); //to access built-in plugins
 
-const CopyWebpackPlugin = require("copy-webpack-plugin"); // copy files from src to dist
+const CopyWebpackPlugin = require('copy-webpack-plugin'); // copy files from src to dist
 
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // extract css to files
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // extract css to files
 
-const IS_DEVELOPMENT = process.env.NODE_ENV === "dev"; // check if we are in dev mode
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin"); // minify images
-const TerserPlugin = require("terser-webpack-plugin"); // minify js
-const { CleanWebpackPlugin } = require("clean-webpack-plugin"); // clean dist folder
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'; // check if we are in dev mode
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin'); // minify images
+const TerserPlugin = require('terser-webpack-plugin'); // minify js
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // clean dist folder
 
-const dirApp = path.join(__dirname, "app"); // src directory
-const dirShared = path.join(__dirname, "shared"); // favicons and stuff
-const dirStyles = path.join(__dirname, "styles"); // styles directory
-const dirNode = "node_modules";
+const dirApp = path.join(__dirname, 'app'); // src directory
+const dirShared = path.join(__dirname, 'shared'); // favicons and stuff
+const dirStyles = path.join(__dirname, 'styles'); // styles directory
+const dirNode = 'node_modules';
 
 module.exports = {
-  entry: [path.join(dirApp, "index.js"), path.join(dirStyles, "index.scss")],
+  entry: [path.join(dirApp, 'index.js'), path.join(dirStyles, 'index.scss')],
   resolve: {
     modules: [dirApp, dirShared, dirStyles, dirNode],
-    extensions: [".ts", ".js", ".glsl"],
+    extensions: ['.ts', '.js', '.glsl'],
   },
   plugins: [
     new webpack.DefinePlugin({
@@ -28,14 +28,14 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: "./shared",
-          to: "",
+          from: './shared',
+          to: '',
         },
       ],
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
+      filename: '[name].css',
+      chunkFilename: '[id].css',
     }),
 
     new CleanWebpackPlugin(),
@@ -49,9 +49,9 @@ module.exports = {
           implementation: ImageMinimizerPlugin.imageminMinify,
           options: {
             plugins: [
-              ["gifsicle", { interlaced: true }],
-              ["jpegtran", { progressive: true }],
-              ["optipng", { optimizationLevel: 5 }],
+              ['gifsicle', { interlaced: true }],
+              ['jpegtran', { progressive: true }],
+              ['optipng', { optimizationLevel: 5 }],
             ],
           },
         },
@@ -65,12 +65,12 @@ module.exports = {
       {
         test: /\.js$/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
       {
         test: /\.ts$/,
-        loader: "ts-loader",
+        loader: 'ts-loader',
       },
 
       {
@@ -79,35 +79,31 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: "",
+              publicPath: '',
             },
           },
           {
-            loader: "css-loader", // css-loader to resolve url() and @imports
+            loader: 'css-loader', // css-loader to resolve url() and @imports
           },
           {
             // postcss-loader to minify and autoprefix our css as well as prefixes css for browser compatibility
-            loader: "postcss-loader",
+            loader: 'postcss-loader',
           },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
           },
         ],
       },
 
       {
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2|webp|fnt)$/,
-        loader: "file-loader",
-
-        options: {
-          name: "[path].[name].[hash].[ext]", // keep original file name to cache it or hash it for production    and so its not cached
-          limit: 10000, // if less than 10 kb, add base64 encoded image to css
-        },
+        test: /\.(jpe?g|svg|png|gif|ico|eot|ttf|woff2?)(\?v=\d+\.\d+\.\d+)?$/i,
+        type: 'asset/resource',
+        dependency: { not: ['url'] },
       },
 
       {
         test: /\.(glsl|frag|vert)$/, // check for glsl, frag, vert files
-        use: ["raw-loader", "glslify-loader"],
+        use: ['raw-loader', 'glslify-loader'],
         exclude: /node_modules/,
       },
     ],
