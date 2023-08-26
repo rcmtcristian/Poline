1da7dbc5fcd89f256cfaa2c62303a3c01302eebe
rcmtcristian <rcmtcristian@gmail.com>
2023-08-25 20:05:21 -0400

    ✨ Feat: added base js Architecture

--- app/index.js
+++ app/index.js
@@ -2,11 +2,13 @@ import Home from './pages/home';
 import Collections from './pages/collections';
 import Detail from './pages/detail';
 import About from './pages/about';
+import { each } from 'neo-async';
 
 class app {
   constructor() {
     this.createContent();
     this.createPages();
+    this.addLinkListeners();
   }
 
   createContent() {
@@ -24,8 +26,54 @@ class app {
 
     this.page = this.pages[this.template];
     this.page.create();
+    this.page.show();
 
     console.log(this.pages);
+  }
+
+  async onChange(href) {
+    const request = new XMLHttpRequest();
+    request.open('GET', href, true);
+    request.onload = () => {
+      if (request.status >= 200 && request.status < 400) {
+        const response = request.responseText;
+        const parser = new DOMParser();
+        const text = parser.parseFromString(response, 'text/html');
+        const content = text.querySelector('.content');
+        const nextTemplate = content.getAttribute('data-template');
+        this.page.hide();
+        this.template = nextTemplate;
+        this.content.setAttribute('data-template', this.template);
+        this.page = this.pages[this.template];
+        this.page.create();
+        this.page.show();
+        history.pushState({}, '', href);
+      } else {
+        console.log('We reached our target server, but it returned an error');
+      }
+    };
+  }
+
+  addLinkListeners() {
+    this.links = document.querySelectorAll('a');
+
+    // this.links.forEach((link) => {
+    //   link.addEventListener('click', (event) => {
+    //     const { href } = link;
+    //     event.preventDefault();
+
+    //     this.onChange(href);
+    //   });
+    // });
+
+    each(links, (link) => {
+      link.onCLick = (event) => {
+        const { href } = link;
+        event.preventDefault();
+
+        this.onChange(href);
+      };
+    });
   }
 }
 
